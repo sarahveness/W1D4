887ec369d67b337aa8dc4e3464729d511cf873ae
Sarah Veness <sarahhveness@gmail.com>
2016-09-08 23:39:25 +0000

    logged array of answer values

---
+++ arraymappingchallenge.js
@@ -0,0 +1,16 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+
+var result = input.map(function(obj) {
+return Math.hypot(obj.x, obj.y);
+});
+
+
+console.log(result);
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
