37b2aebb0a0a498da048644e912b6cab035fab53
Sarah Veness <sarahhveness@gmail.com>
2016-09-08 19:46:56 +0000

    changed for loop to forEach loop

--- callback.js
+++ callback.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+  arr.forEach(function(value, i) {
     if (arr[i] == "Waldo") {
       found(i);   // execute callback
     }
-  }
+  });
 }
 
 
